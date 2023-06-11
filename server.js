6b10012b9221ad87ab57615df0b530f4d7df921f
saanspeur <evelin.ortega018@gmail.com>
2023-06-10 19:54:03 -0700

    get /notes and send notes.html file

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
 );
