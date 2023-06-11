c538a781032b19ab553b85e7e37c7f34afe1d672
saanspeur <evelin.ortega018@gmail.com>
2023-06-10 19:58:34 -0700

    get save button on front end to save notes / console log on back end

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.get("/api/notes", (req, res) => {
     res.json(savednotes)
 })
 
+app.post("/api/notes", (req, res) => {
+    const savednotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    console.log("hello this is line 30 server.js ", req.body)
+    savednotes.push(req.body)
+    fs.writeFileSync("./db/db.json", JSON.stringify(savednotes))
+    res.send("Success")
+})
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
